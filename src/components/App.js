a55a7d31e89829cc161b6daff0b7bb2ebf51d504
Prashant Kumar <prashant.aman9@gmail.com>
2024-08-01 12:01:17 +0530

    Focus textarea on mount using useRef hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 // import remarkGfm from 'remark-gfm'
 
 import '../styles/App.css'
 
 function App() {
-	const [markdown, setMarkdown] = useState('')
+    const [markdown, setMarkdown] = useState('')
+    const ref = useRef(null)
+    
+    useEffect(() => {
+        ref.current.focus()
+    }, [])
+
 	return (
 		<div className='app'>
-			<textarea
+            <textarea
+                ref={ref}
 				value={markdown}
 				onChange={(e) => setMarkdown(e.target.value)}
 				className='textarea'
